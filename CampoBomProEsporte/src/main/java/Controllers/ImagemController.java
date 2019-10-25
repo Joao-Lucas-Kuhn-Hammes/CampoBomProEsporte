ffd31399f5fb6b12bdfccf90c764281dcf5f341c
JoaoLucas <JoaoLucas@DESKTOP-BF38NUG>
2019-10-24 21:05:55 -0300

    asd

--- CampoBomProEsporte/src/main/java/Controllers/ImagemController.java
+++ CampoBomProEsporte/src/main/java/Controllers/ImagemController.java
@@ -1,8 +1,14 @@
 package Controllers;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
+@RestController
+@RequestMapping("/imagem/")
 
 public class ImagemController {
 	@GetMapping()
