e55faf9621519f182383fa7e1be578ef55731581
JoaoLucas <joaolucaskuhnahammes@gmail.com>
2019-11-27 22:42:30 -0200

    mudancinhas

--- CampoBomProEsporte/src/main/java/Persistencia/EsporteDAO.java
+++ CampoBomProEsporte/src/main/java/Persistencia/EsporteDAO.java
@@ -5,7 +5,6 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 import Models.Esporte;
-import Models.Usuario;
 
 import com.mysql.jdbc.PreparedStatement;
 
--- CampoBomProEsporte/src/main/java/Persistencia/LocalDAO.java
+++ CampoBomProEsporte/src/main/java/Persistencia/LocalDAO.java
@@ -4,7 +4,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import Models.Local;
-import Models.Usuario;
 
 import com.mysql.jdbc.PreparedStatement;
 
